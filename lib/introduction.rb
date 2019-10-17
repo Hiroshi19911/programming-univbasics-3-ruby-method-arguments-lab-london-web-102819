b9b5f5a2dc3a8164ad881a465e50bf384ff20b44
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-17 14:20:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,8 +9,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
-def introduction_with_language("Dan", "Ember.js")
+def introduction_with_language()
   puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end 
 
